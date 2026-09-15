a46f76fc2d54b985ba57f0ff4d24ed8ef8f05fbe
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-15 13:29:43 -0600

    fixing_input.py

---
+++ pie.py
@@ -0,0 +1,10 @@
+#LK6, pie.py
+
+everything = 42
+
+pie = 3.145
+
+print(f"the varibles are {pie} and it is saved at {id(pie)}")
+
+print(f"the varibles are {everything} and it is saved at {id(everything)}")
+

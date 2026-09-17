0b636d5f4fb1a066f31a3e5cfdcc2e0e4a6fa481
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-17 13:50:27 -0600

    your_budget.py

---
+++ your_budget.py
@@ -0,0 +1,12 @@
+#LK6, your budget
+
+income = float(input("what is your monthly income:"))
+
+rent = float(input("what is your rent monthly:"))
+
+util = float(input("what do you pay monthly:"))
+
+groc = float(input("what do yo pay for goceries monthly:"))
+
+tran = float(input("what is your monthly payment to travel"))
+    

28ec082d8c97b66737003a668759574559ee23ee
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-18 20:29:37 +0000

    your_budget.py

--- your_budget.py
+++ your_budget.py
@@ -4,13 +4,19 @@ income = float(input("what is your monthly income:$"))
 
 rent = float(input("what is your rent monthly:$"))
 
-util = float(input("what do you pay monthly:$"))
+util = float(input("what do you pay monthly for utilities:$"))
 
-groc = float(input("what do yo pay for goceries monthly:$"))
+groc = float(input("what do yo pay for groceries monthly:$"))
 
 tran = float(input("what is your monthly payment to travel:$"))
 
-print (f"{rent/income*100}")
-print (f"{util/income*100}")
-print (f"{groc/income*100}")
-print (f"{tran/income*100}")
+
+print(f"your rent is {rent/income*100}% of your income")
+
+print(f"your utilities is {util/income*100}% of your income")
+
+print(f"your groceries is {groc/income*100}% of your income")
+
+print(f"your transportation is {tran/income*100}% of your income")
+
+print(f"this is your spending money for the month: ${income-rent-util-groc-tran}")

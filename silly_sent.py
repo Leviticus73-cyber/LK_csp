272df0c340f3cf89225b7aeec07d688771a80cd1
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-21 13:52:27 -0600

    silly_sent.py

---
+++ silly_sent.py
@@ -0,0 +1,17 @@
+#LK6, silly sentences
+
+print ("this is a madlib")
+
+plu1= input("PLURAL NOUN:")
+perin= input("LAST NAME OF A PERSON IN THE ROOM:")
+agje1= input("ADJECTIVE:")
+noun1= input("NOUN:")
+noun2= input("NOUN:")
+part1= input("PART OF BODY:")
+part2= input("PART OF BODY:")
+plu2= input("PLURAL NOUN:")
+noun3= input("NOUN:")
+= input(":")
+= input(":")
+= input(":")
+= input(":")

39f4c9daddd768b5dba68e0b4e09d96e212bbbef
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-23 13:53:31 -0600

    pass_word_checker.py

---
+++ pass_word_checker.py
@@ -0,0 +1,22 @@
+#LK6, pass word checker
+print ("your Password must contain least have 8 characters long, needs at least one Uppercase, at least one lowercase, needs at least one Number, and must have at least one Symbol")
+al8c = False
+low = False
+num = False
+sym = False
+w = 0
+pas = input("type in your Password:")
+lent =len(pas)
+
+
+
+
+
+if lent >= 8:
+    al8c = True
+    if al8c == True:
+        (w + 1)
+if pas
+
+    
+    

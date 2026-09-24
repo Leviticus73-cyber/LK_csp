3694eca199eb127891cb5626da7e3080408584af
Leviticus73-cyber <levi.king@ucas-edu.net>
2026-09-24 13:52:00 -0600

    pass_word_checker.py

---
+++ loop.py
@@ -0,0 +1,28 @@
+#LK6, Loops
+
+count = 1
+
+
+while count <= 10:
+    print (count)
+    count += 1
+
+    break
+
+
+
+import random
+
+goose = random.randint(1,99999)
+duck = 1
+
+while True:
+    print("Duck!")
+    duck += 1
+    if duck == goose:
+        break
+print("Goose!")
+print(duck)
+
+
+sibling =["caleb", "marissa", "chloe", "kam", "remmy", "andy", "daisy", "unknown"]
--- pass_word_checker.py
+++ pass_word_checker.py
@@ -1,10 +1,12 @@
 #LK6, pass word checker
 print ("your Password must contain least have 8 characters long, needs at least one Uppercase, at least one lowercase, needs at least one Number, and must have at least one Symbol")
 al8c = False
+up = False
 low = False
 num = False
 sym = False
-w = 0
+symbols = "!@#$%^&*"
+score = 0
 pas = input("type in your Password:")
 lent =len(pas)
 
@@ -14,9 +16,30 @@ lent =len(pas)
 
 if lent >= 8:
     al8c = True
-    if al8c == True:
-        (w + 1)
-if pas
 
-    
-    
+for letter in pas:
+    if letter.isupper():
+        up==True
+    if letter in symbols:
+        sym==True
+    if letter.islower():
+        low==True
+    if letter.isnumeric():
+        num==True
+if al8c == True:
+        score += 1
+if up == True:
+    score += 1
+if low == True:
+        score += 1
+if num == True:
+        score += 1
+if sym == True:
+        score += 1
+if score == 5:
+     strong = "strong"
+elif score >= 3:
+    strong = "medium"
+else: 
+     strong = "weak"
+print ("your password is "+strong+".")
